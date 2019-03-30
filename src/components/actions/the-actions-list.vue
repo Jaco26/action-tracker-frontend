9329268c22d6c4d608fffebbf52ce59b11411281
Jacob Albright <jacob.albright23@gmail.com>
2019-03-29 20:23:27 -0500

    fixed date so that it now defaults to local date. added minimal dashboard summary at top of reports page

--- src/components/actions/the-actions-list.vue
+++ src/components/actions/the-actions-list.vue
@@ -1,9 +1,32 @@
 <template>
   <b-card>
     <b-form-input type="date" v-model="d1"></b-form-input>
-    <pre>
-      {{dateAndCategory}}
-    </pre>
+    <b-card-text>
+      <b-form-row>
+        <b-col 
+          v-for="category in categories"
+          :key="category"
+          style="height: 50px; min-width: 180"
+        >
+         {{category}}: {{dateAndCategory[category].length}}
+        </b-col>
+      </b-form-row>   
+      <!-- <b-row 
+        v-for="category in categories"
+        :key="category"
+      >
+        <strong>{{category}}</strong>
+        <b-col v-for="(item, i) in dateAndCategory[category]" :key="i">
+          {{item.displayDate}} <br>
+          {{item.description}}
+        </b-col>
+      </b-row> -->
+
+
+      <pre>
+        {{dateAndCategory}}
+      </pre>
+    </b-card-text>
   </b-card>
 </template>
 
@@ -21,13 +44,18 @@ export default {
     ...mapGetters('myActions', [
       'dateAndCategory',
     ]),
- 
+    categories() {
+      if (this.dateAndCategory) {
+        return Object.keys(this.dateAndCategory)
+      }
+      return [];
+    }
   },
   mounted() {
     const d = new Date();
-    const year = (d.getUTCFullYear()).toString();
-    let month = (d.getUTCMonth() + 1).toString();
-    let day = (d.getUTCDate()).toString();
+    const year = (d.getFullYear()).toString();
+    let month = (d.getMonth() + 1).toString();
+    let day = (d.getDate()).toString();
     month = month.length === 1 ? `0${month}` : month;
     day = day.length === 1 ? `0${day}` : day;
     this.d1 = `${year}-${month}-${day}`;
